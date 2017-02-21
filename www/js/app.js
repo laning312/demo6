3191ec0f4659638aac8d115db8e77aaac7c75374
laning312 <286046620@qq.com>
2017-02-21 14:37:25 +0800

    一个ionic选项卡demo

---
+++ www/js/app.js
@@ -0,0 +1,63 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+  .config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state("tabs", {
+        url: "/tabs",
+        templateUrl: "views/tabs/tabs.html"
+      })
+      .state("tabs.home", {
+        url: "/home",    //这是一个子路由，所以会添加到父路由之后
+        views: {
+          "home-tab": {
+            templateUrl: "views/home/home.html"
+          }
+        }
+      })
+      .state("tabs.video", {
+        url: "/video",
+        views: {
+          "video-tab": {
+            templateUrl: "views/video/video.html"
+          }
+        }
+      })
+      .state("tabs.topic", {
+        url: "/topic",
+        views: {
+          "topic-tab": {
+            templateUrl: "views/topic/topic.html"
+          }
+        }
+      })
+      .state("tabs.my", {
+        url: "/my",
+        views: {
+          "my-tab": {
+            templateUrl: "views/my/my.html"
+          }
+        }
+      });
+    $urlRouterProvider.otherwise("/tabs");
+  }])
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
